f43c65cd65c621f9e57a6b8dfc8314c571c43500
Ethan <stewaret@clarkson.edu>
2024-03-11 18:52:56 -0400

    More work on front-end, added credential files locally

--- WebUI.py
+++ WebUI.py
@@ -5,12 +5,12 @@ Created on Mon Mar 11 17:29:50 2024
 @author: Riley Outlaw, Ethan Stewart
 """
 
-from flask import Flask
+from flask import Flask, render_template
 webUI = Flask(__name__)   # Flask constructor
 
 @webUI.route('/')
 def index():
-    return "Blah"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     webUI.run()
---
+++ search.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 18:31:12 2024
+
+@author: ethan
+"""
+
